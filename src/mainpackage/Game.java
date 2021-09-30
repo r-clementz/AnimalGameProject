11e5d2cdd7f53a0e40ba6a756b29718c7816d704
r-clementz <rie.h620@gmail.com>
2021-09-30 14:24:24 +0200

    createPlayer method idea
    commented code idea for createPlayer method in Game class

--- src/mainpackage/Game.java
+++ src/mainpackage/Game.java
@@ -1,9 +1,54 @@
 package mainpackage;
 
+import java.util.Scanner;
+
 public class Game {
-    public Game(){}
-    public void newGame(){
+    public Scanner console;
+    private int numberOfPlayer;
+    private int numberOfRound;
+    public int [] playersChoices;
+    private Store store;
+    private Animal animal;
+   // public Player[] players;
+    public Game(){
+        console= new Scanner (System.in);
+        store = new Store();
+        playersChoices = new int[3];
+        //players = new Player[3];
+
 
     }
+    public void newGame(){
+        //Round 5-30
+
+    }
+//    public void createPlayers(int numberOfPlayer){ // IDEA
+//        switch(numberOfPlayer){
+//            case 2:
+//                players[1]= new Player("player1");
+//                players[1].giveNickName();
+//                players[2]= new Player("player2");
+//                players[2].giveNickName();
+//                break;
+//            case 3:
+//                players[1]= new Player("player1");
+//                players[1].giveNickName();
+//                players[2]= new Player("player2");
+//                players[2].giveNickName();
+//                players[3]= new Player("player2");
+//                players[3].giveNickName();
+//                break;
+//            case 4:
+//                players[1]= new Player("player1");
+//                players[1].giveNickName();
+//                players[2]= new Player("player2");
+//                players[2].giveNickName();
+//                players[3]= new Player("player2");
+//                players[3].giveNickName();
+//                players[4]= new Player("player1");
+//                players[4].giveNickName();
+//                break;
+//        }
+//    }
 
 }
